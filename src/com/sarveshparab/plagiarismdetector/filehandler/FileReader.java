578b0881d29bb089c595ade9d829a2bce4f21561
sarvesh Parab <sarveshsparab@gmail.com>
2018-11-04 23:12:14 -0800

    simple clean up

--- src/com/sarveshparab/plagiarismdetector/filehandler/FileReader.java
+++ src/com/sarveshparab/plagiarismdetector/filehandler/FileReader.java
@@ -7,7 +7,6 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -16,7 +15,7 @@ public class FileReader {
 
     public List<String> readFromFile(String fileName){
         Path path = Paths.get(fileName);
-        List<String> fileLines = new ArrayList<>();
+        List<String> fileLines;
 
         try (Stream<String> fileStream = Files.lines(path)) {
             fileLines = fileStream
