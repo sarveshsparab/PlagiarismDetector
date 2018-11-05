cbb235b2b549c4d21a0af4da353a8de9fb39449c
sarvesh Parab <sarveshsparab@gmail.com>
2018-11-05 01:11:01 -0800

    created a static strings class

---
+++ src/com/sarveshparab/plagiarismdetector/util/PDUtilStrings.java
@@ -0,0 +1,12 @@
+package com.sarveshparab.plagiarismdetector.util;
+
+public class PDUtilStrings {
+
+    public static final String FILE_1 = "file1";
+    public static final String FILE_2 = "file2";
+    public static final String SYNONYM_FILE = "synonym";
+    public static final String TUPLE_SIZE = "tuple";
+
+    public static final String DEFAULT_TUPLE_SIZE = "3";
+
+}
